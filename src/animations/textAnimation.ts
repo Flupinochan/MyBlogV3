14f513895bdc28d56116e10252db60d139942644
Flupinochan <flupino@metalmental.net>
2025-04-13 17:02:49 +0900

    refix

--- src/animations/textAnimation.ts
+++ src/animations/textAnimation.ts
@@ -7,9 +7,12 @@ gsap.registerEffect({
   name: 'textAnimation',
   extendTimeline: false,
   effect: (_elements: gsap.TweenTarget, _config: TextAnimationConfig = {}) => {
+    const mm = gsap.matchMedia();
 
-    const renderText = "MetalMental is a Full-Stack and SRE Engineer";
-    const renderText2 = `
+    // PCだけアニメーション
+    mm.add("(min-width: 769px)", () => {
+      const renderText = "MetalMental is a Full-Stack and SRE Engineer";
+      const renderText2 = `
       <span style="color: var(--bg-color);">MetalMental</span>
       is a
       <span style="color: var(--bg-color);">Full-Stack</span>
@@ -17,37 +20,44 @@ gsap.registerEffect({
       <span style="color: var(--bg-color);">SRE</span>
       Engineer
     `;
-    const renderText3 = "フロントエンドからバックエンド、インフラまで手掛けるメタルなメンタルを持つエンジニアです";
+      const renderText3 = "フロントエンドからバックエンド、インフラまで手掛けるメタルなメンタルを持つエンジニアです";
 
-    const tl = gsap.timeline();
-    tl.set(".contactMeAnimation", {
-      opacity: 0,
-    }).to(".h1TextAnimation", {
-      text: {
-        value: renderText,
-        delimiter: '',
-        // speed: 0.5, // 1文字を表示する間隔 ※durationは自動で計算
-      },
-      ease: 'power2.out',
-      duration: 1.5, // 文字全体が表示されるまでの時間 ※speedは自動で計算
-    }).to(".pTextAnimation", {
-      text: {
-        value: renderText3,
-        delimiter: '',
-      },
-      ease: 'power2.out',
-      delay: 0.3,
-      duration: 1,
-    }).to(".h1TextAnimation", {
-      text: {
-        value: renderText2,
-        delimiter: '',
-      },
-      duration: 0,
-      delay: 1,
-    }).to(".contactMeAnimation", {
-      opacity: 1,
-      duration: 0.5,
+      const tl = gsap.timeline();
+      tl.set(".contactMeAnimation", {
+        opacity: 0,
+      }).set(".h1TextAnimation", {
+        text: "",
+      }).set(".pTextAnimation", {
+        text: "",
+      }).to(".h1TextAnimation", {
+        text: {
+          value: renderText,
+          delimiter: '',
+          // speed: 0.5, // 1文字を表示する間隔 ※durationは自動で計算
+        },
+        ease: 'power2.out',
+        duration: 1.5, // 文字全体が表示されるまでの時間 ※speedは自動で計算
+      }).to(".pTextAnimation", {
+        text: {
+          value: renderText3,
+          delimiter: '',
+        },
+        ease: 'power2.out',
+        delay: 0.3,
+        duration: 1,
+      }).to(".h1TextAnimation", {
+        text: {
+          value: renderText2,
+          delimiter: '',
+        },
+        duration: 0,
+        delay: 1,
+      }).to(".contactMeAnimation", {
+        opacity: 1,
+        duration: 0.5,
+      });
+
+      return () => { };
     });
 
   }
