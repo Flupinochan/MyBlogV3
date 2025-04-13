6672d8f0b749cc02c85299e640af761847fb75f5
Flupinochan <flupino@metalmental.net>
2025-04-13 20:04:07 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./layouts/header/Header";
 import Footer from "./layouts/footer/Footer";
