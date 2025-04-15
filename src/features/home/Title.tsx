9add29655cbbfc6d9f653c1bfa0d94101c094637
Flupinochan <flupino@metalmental.net>
2025-04-15 20:29:51 +0900

    rollback

--- src/features/home/Title.tsx
+++ src/features/home/Title.tsx
@@ -11,7 +11,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Title = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const isMobile = useMediaQuery("(max-width: 768px)");
 
   useGSAP(() => {
     gsap.effects.fadeIn(ref.current, { scope: ref.current });
@@ -20,64 +19,28 @@ const Title = () => {
   }, { scope: ref });
 
   return (
-    <Grid ref={ref} gutter={0} grow className={`${titleStyles.titleGrid} fadeIn`}>
-      {isMobile ? (
-        // モバイル：画像を上に、テキストを下にレンダリング
-        <>
-          <GridCol span={12} className={titleStyles.secondCol}>
-            <Image src={titleImage} alt="title image" onLoad={() => ScrollTrigger.refresh()} />
-          </GridCol>
-          <GridCol span={12} className={titleStyles.firstCol}>
-            <Stack justify="space-around" style={{ height: "100%" }}>
-              <Stack>
-                <h1 className={`${titleStyles.titleText} h1TextAnimation`}><span className={indexStyles.purple}>MetalMental</span> is a <span className={indexStyles.purple}>Full-Stack</span> and <span className={indexStyles.purple}>SRE</span> engineer</h1>
-                <p style={{ paddingTop: "1rem" }} className="pTextAnimation">
-                  フロントエンドからバックエンド、インフラまで手掛けるメタルなメンタルを持つエンジニアです
-                </p>
-              </Stack>
-              <div className={`${titleStyles.button} contactMeAnimation`}>
-                <CustomAnchor href="#contact" text="Contact Me" />
-              </div>
-              <Flex justify="end" align="center" className={`${titleStyles.miniText} contactMeAnimation`}>
-                <div className={titleStyles.square} />
-                <p>
-                  Currently working on{" "}
-                  <span className={indexStyles.purple}>generative AI</span>
-                </p>
-              </Flex>
-            </Stack>
-          </GridCol>
-        </>
-      ) : (
-        // PC：テキストを左に、画像を右にレンダリング
-        <>
-          <GridCol span={{ base: 12, md: 8 }} className={titleStyles.firstCol}>
-            <Stack justify="space-around" style={{ height: "100%" }}>
-              <Stack>
-                <h1 className={`${titleStyles.titleText} h1TextAnimation`}><span className={indexStyles.purple}>MetalMental</span> is a <span className={indexStyles.purple}>Full-Stack</span> and <span className={indexStyles.purple}>SRE</span> engineer</h1>
-                <p style={{ paddingTop: "1rem" }} className="pTextAnimation">
-                  フロントエンドからバックエンド、インフラまで手掛けるメタルなメンタルを持つエンジニアです
-                </p>
-              </Stack>
-              <div className={`${titleStyles.button} contactMeAnimation`}>
-                <CustomAnchor href="#contact" text="Contact Me" />
-              </div>
-              <Flex justify="end" align="center" className={`${titleStyles.miniText} contactMeAnimation`}>
-                <div className={titleStyles.square} />
-                <p>
-                  Currently working on{" "}
-                  <span className={indexStyles.purple}>generative AI</span>
-                </p>
-              </Flex>
-            </Stack>
-          </GridCol>
-          <GridCol span={{ base: 12, md: 4 }} className={titleStyles.secondCol}>
-            <Image src={titleImage} alt="title image" onLoad={() => ScrollTrigger.refresh()} />
-          </GridCol>
-        </>
-      )}
-    </Grid>
-  );
-};
+    <Grid ref={ref} gutter={0} grow className={`${titleStyles.titleGrid} fadeIn`}
+      breakpoints={{ xs: '0px', sm: '0px', md: '768px', lg: '0px', xl: '0px' }}>
+      <GridCol span={{ base: 12, md: 8 }} className={titleStyles.firstCol}>
+        <Stack justify="space-around" style={{ height: "100%" }}>
+          <Stack>
+            <h1 className={`${titleStyles.titleText} h1TextAnimation`}><span className={indexStyles.purple}>MetalMental</span> is a <span className={indexStyles.purple}>Full-Stack</span> and <span className={indexStyles.purple}>SRE</span> engineer</h1>
+            <p style={{ paddingTop: "1rem" }} className="pTextAnimation">フロントエンドからバックエンド、インフラまで手掛けるメタルなメンタルを持つエンジニアです</p>
+          </Stack>
+          <div className={`${titleStyles.button} contactMeAnimation`}>
+            <CustomAnchor href="#contact" text="Contact Me" />
+          </div>
+          <Flex justify={"end"} align={"center"} className={`${titleStyles.miniText} contactMeAnimation`}>
+            <div className={titleStyles.square} />
+            <p>Currently working on <span className={indexStyles.purple}>generative AI</span></p>
+          </Flex>
+        </Stack>
+      </GridCol>
+      <GridCol span={{ base: 12, md: 4 }} className={titleStyles.secondCol}>
+        <Image src={titleImage} alt="title image" />
+      </GridCol>
+    </Grid >
+  )
+}
 
-export default Title;
+export default Title
