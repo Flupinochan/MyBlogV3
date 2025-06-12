aba232c7bff4613d1373b6abfe4985299231f9ca
Flupinochan <flupino@metalmental.net>
2025-06-12 21:43:04 +0900

    register workbox

--- src/service-worker/init.ts
+++ src/service-worker/init.ts
@@ -1,12 +1,16 @@
+import { Workbox } from 'workbox-window';
+
 export const initServiceWorker = async () => {
   // service workerがサポートされている場合
   if ('serviceWorker' in navigator) {
     try {
       // service workerを登録
-      await navigator.serviceWorker.register('/serviceWorker.js', {
-        scope: "/",
-        type: "module"
-      });
+      // await navigator.serviceWorker.register('/serviceWorker.js', {
+      //   scope: "/",
+      //   type: "module"
+      // });
+      const wb = new Workbox('/serviceWorker.js');
+      wb.register();
     } catch (error) {
       console.error('Service Worker registration failed:', error);
     }
