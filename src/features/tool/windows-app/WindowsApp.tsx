c4124fd1d3ce44c408ce89eb055c9f93d2694b8c
Flupinochan <flupino@metalmental.net>
2025-09-04 20:31:29 +0900

    windowsアプリのリンクを修正

--- src/features/tool/windows-app/WindowsApp.tsx
+++ src/features/tool/windows-app/WindowsApp.tsx
@@ -1,5 +1,5 @@
-import { Anchor, List, ListItem, Stack } from "@mantine/core"
-import H4 from "../../../components/H4"
+import { Anchor, List, ListItem, Stack } from "@mantine/core";
+import H4 from "../../../components/H4";
 import toolStyles from "../Tool.module.css";
 
 const WindowsApp = () => {
@@ -8,13 +8,17 @@ const WindowsApp = () => {
       <H4 text="Windows App" />
       <List className={toolStyles.listMarker} withPadding>
         <ListItem>
-          <Anchor target="_blank" rel="noopener noreferrer" href="ms-windows-store://pdp/?productid=9NW60Q3D6FMH&cid=PCCongratsBnr" >
+          <Anchor
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.microsoft.com/store/apps/9NW60Q3D6FMH"
+          >
             FrameResizer
           </Anchor>
         </ListItem>
       </List>
     </Stack>
-  )
-}
+  );
+};
 
-export default WindowsApp
+export default WindowsApp;
